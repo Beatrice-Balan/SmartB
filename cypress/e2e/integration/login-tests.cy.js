be18cab32077f2a0bfe37ff5b34b7fa1b97a7170
Beatrice-Balan <beatrice.oana.balan@gmail.com>
2025-05-18 10:47:10 -0500

    first commit smartB

---
+++ cypress/e2e/integration/login-tests.cy.js
@@ -0,0 +1,64 @@
+/// <reference types="cypress"/>
+import BasePage from "../../pages/BasePage";
+import LoginPage from "../../pages/LoginPage";
+
+describe('Login Verification', () => {
+
+  const loginPage = new LoginPage();
+  const basePage = new BasePage();
+
+  beforeEach(() => {
+    cy.visit(Cypress.env("APP_BASE_URL"));
+  })
+
+  it('TG11S-T131 -Validate login with valid credentials', () => {
+
+    loginPage.login(Cypress.env('USER'), Cypress.env('PASSWORD'));
+
+    cy.url().should('include', 'weborders');
+
+    basePage.getWebOrdersHeading().should('have.text', 'Web Orders');
+    basePage.getLogoutButton().should('have.text', 'Logout');
+    basePage.getWelcomeUserInfo().should('include.text', Cypress.env('USER'));
+  })
+
+ it("TG11S-T142 - Validate logout", () => {
+
+    loginPage.login(Cypress.env("USER"), Cypress.env("PASSWORD"));
+    basePage.getLogoutButton().click();
+    cy.url().should("include", "Login");
+    loginPage.getLoginForm().should("be.visible");
+  });
+
+  [
+    {
+      title: 'emptyUsernameAndPassword',
+      username: "",
+      password: "",
+    },
+    {
+      title: 'invalidUsernameValidPassword',
+      username: "InvalidUsername",
+      password: Cypress.env("PASSWORD"),
+    },
+    {
+      title: 'validUsernameInvalidPassword',
+      username: Cypress.env("USER"),
+      password: "InvalidPassword",
+    },
+    {
+      title: 'invalidUsernameAndPassword',
+      username: "InvalidUsername",
+      password: "InvalidPassword",
+    },
+  ].forEach((creds) => {
+    it(`TG11S-T164 - Validate login with invalid credentials ${creds.title}`, () => {
+
+      if(creds.username === '' && creds.password === '') loginPage.clickOnLoginButton();
+      else loginPage.login(creds.username, creds.password);
+
+      cy.url().should("include", "login");
+      loginPage.getErrorMessage().should('have.text', 'Invalid Login or Password.');
+    });
+  });
+});
---
+++ cypress/pages/LoginPage.js
@@ -0,0 +1,41 @@
+export default class LoginPage {
+
+  getLoginForm() {
+    return cy.get('#aspnetForm')
+  }
+
+  getUsernameInput() {
+    return cy.get("#ctl00_MainContent_username");
+  }
+
+  getPasswordInput() {
+    return cy.get("#ctl00_MainContent_password");
+  }
+
+  getLoginButton() {
+    return cy.get("#ctl00_MainContent_login_button");
+  }
+
+  getErrorMessage() {
+    return cy.get("#ctl00_MainContent_status");
+  }
+
+  // Reusable methods for login page
+  enterUsername(user) {
+    this.getUsernameInput().type(user);
+  }
+
+  enterPassword(password) {
+    this.getPasswordInput().type(password);
+  }
+
+  clickOnLoginButton() {
+    this.getLoginButton().click();
+  }
+
+  login(user, password) {
+    this.enterUsername(user);
+    this.enterPassword(password);
+    this.clickOnLoginButton();
+  }
+}
